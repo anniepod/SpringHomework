60510fe7a9b4c4367192adf1d37cf92c84c961be
anniepod <31967599+anniepod@users.noreply.github.com>
2018-03-28 01:43:31 -0400

    HW 11 1-4

--- Chapter11 HW.py
+++ Chapter11 HW.py
@@ -10,12 +10,55 @@ where n is a whole number integer and n = |start - stop| / step
 """
 
 #Question 2
-"Consider this fragment of code:
+"""Consider this fragment of code:
 
 import turtle
 
 tess = turtle.Turtle()
 alex = tess
 alex.color("hotpink")
+
 Does this fragment create one or two turtle instances? Does setting the
-color of alex also change the color of tess? Explain in detail."
+color of alex also change the color of tess? Explain in detail."""
+
+#This fragment of code will only produce one turtle because alex is tess. The
+#variable tess was aliased and both names reference the same turtle. Changes to one
+#will result in changes to another.
+
+
+
+#Question 3
+
+
+a = [1, 2, 3]
+b = a[:]
+b[0]= 5
+
+#  before
+#       a --> [1,2,3]
+#       b --> [1,2,3]
+#  after
+#       a --> [1,2,3]
+#       b --> [5,2,3]
+#In this case, a is not an alias for b
+#and changes to b do not affect that of a
+
+
+#Question 4
+
+this = ["I", "am", "not", "a", "crook"]
+that = ["I", "am", "not", "a", "crook"]
+print("Test 1: {0}".format(this is that))
+that = this
+print("Test 2: {0}".format(this is that))
+
+#The result will be
+#Test 1: False
+#Test 2: True
+
+# Although this and that are lists with identical elements, they are different ones
+# since "this is that" is not true, it will print Test 1: False
+#Only after they are made aliases in Line 4, will "this is that" be true and
+#yield Test 2: True as an outcome after formatting the function
+
+
