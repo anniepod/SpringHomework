6dbe81f3ea6510ae610e96302377f4320a8651b6
anniepod <31967599+anniepod@users.noreply.github.com>
2018-03-28 15:03:27 -0400

    HW 11 1-4

--- Chapter11 HW.py
+++ Chapter11 HW.py
@@ -62,3 +62,25 @@ print("Test 2: {0}".format(this is that))
 #yield Test 2: True as an outcome after formatting the function
 
 
+
+
+#Question 6
+
+def scalar_mult(s,v):
+    for i in range(len(v)):
+        v[i] = v[i]*s
+    return v
+
+print(scalar_mult(5,[1,2]))
+print(scalar_mult(3, [1, 0, -1]))
+
+
+#Question 10
+
+def replace(s, old, new):
+    x = s.split(old)
+    g = new
+    new = g.join(x)
+    return new
+
+print(replace("Mississippi", "i", "I"))
