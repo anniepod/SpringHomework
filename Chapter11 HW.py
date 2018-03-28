7685488b894dc9c766f25d39f2218ec706a0fab7
anniepod <31967599+anniepod@users.noreply.github.com>
2018-03-28 01:18:35 -0400

    HW 11 new

---
+++ Chapter11 HW.py
@@ -0,0 +1,12 @@
+#Question 1
+"""What is the Python interpreter’s response to the following? >>> list(range(10, 0, -2))
+The three arguments to the range function are start, stop, and step, respectively. In this
+example, start is greater than stop. What happens if start < stop and step < 0? Write a
+rule for the relationships among start, stop, and step."""
+
+"""
+[start+(1*step), start+(2*step), start+(3*step) ... ] until (n*step) = stop 
+where n is a whole number integer and n = |start - stop| / step
+"""
+
+#Question 2
