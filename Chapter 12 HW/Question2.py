437b46c0b254ffa48a75fbd1369d4db517a19c16
anniepod <31967599+anniepod@users.noreply.github.com>
2018-04-23 23:11:17 -0400

    HW 11 finish

---
+++ Chapter 12 HW/Question2.py
@@ -0,0 +1,17 @@
+import math
+
+#part a
+
+#lots of functions
+
+#part b
+
+#math.ceil returns the smallest integer greater than or equal to a number
+#math.floor returns the largest integer less than or equal to a given number
+
+#part c
+#we've been computing the square root by raising a number to the 1/2 power (x)**exponent
+
+#part d
+
+#pi and e are the two data constants in the module
